cfdcc932f9725064d29523a0d9f75190db005cc9
Alex Schedler <con.sume.org+github@googlemail.com>
2018-07-31 09:36:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,24 +10,17 @@ const   Koa = require("koa"),
 ██║  ██╗╚██████╔╝██║  ██║
 ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝
 */
-// a server with koa... wooa, something new!
-const app = new Koa();
-app.use( 
-    r.get('/', async (ctx) => {
-        await send(ctx, 'index.html', { root: __dirname + '/public' });
-    })
-);
-
-app.use( 
-    r.get('/png', async (ctx) => {
-        let url = ctx.request.query.url || 'https://www.source.horse';
-        let options = JSON.parse( ctx.request.query.options || null );
-        let download = ctx.request.query.download || false;
-        let filename = new Buffer.from(url).toString('base64');
+const shoot = async function(ctx){
+        console.log(ctx.path);
+        const url = ctx.request.query.url || 'https://www.source.horse';
+        const options = JSON.parse( ctx.request.query.options || null );
+        const download = ctx.request.query.download || false;
+        const filename = new Buffer.from(url).toString('base64');
+        const type = ctx.path.indexOf('png')>-1?'png':'jpg';
         
         let minimal = {
             device: 'iMac 27',
-            type: 'png',
+            type: type,
             fullPage: true,
             omitBackground: false,
             tmpOpts: {
@@ -39,7 +32,7 @@ app.use(
         options = Object.assign({}, minimal, options);
         console.info('Shooting', url, 'with options', options);
         const tmpStream = await browserless.screenshot(url, options)
-        
+
         if( download === false ){
             await send(ctx, `screenshots/${filename}.png`, { root: __dirname + '/public' });
             console.log('file delivered (inline)');
@@ -50,8 +43,17 @@ app.use(
             console.log(`file delivered (download as "${download}")`);    
             tmpStream.cleanupSync() // It removes the file!
         }
+}
+
+const app = new Koa();
+app.use( 
+    r.get('/', async (ctx) => {
+        await send(ctx, 'index.html', { root: __dirname + '/public' });
     })
 );
+app.use( r.get('/png', shoot) );
+app.use( r.get('/jpg', shoot) );
+app.use( r.get('/jpeg', shoot) );
 // launch our server on the ENV Port or 3000...
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
